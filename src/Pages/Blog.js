5c1e40e9318740914e5cbf1b3132e1e6026af9e6
Robert Askarov <71882899+RobertDeveloperBlog@users.noreply.github.com>
2022-09-23 00:00:33 +0300

    blog edited

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,7 +1,59 @@
 import React, { Component } from 'react'
+import { Container, Card, ListGroup, Col } from 'react-bootstrap'
 
 export default class Blog extends Component {
   render() {
-    return <div>Blog</div>
+    return (
+      <Container>
+        <div className="d-flex align-items-center mt-5">
+          <div className="flex-shrink-0">
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2019%2F01%2FJavaScript_Logo.png&f=1&nofb=1"
+              alt="js"
+              width="150"
+              height="150"
+            />
+          </div>
+          <div className="flex-grow-1 ms-3 ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste nisi
+            beatae soluta vero esse voluptatibus ratione commodi, deleniti,
+            maiores sequi eaque nemo eligendi officia illum perspiciatis
+            exercitationem rerum minus error?
+          </div>
+        </div>
+        <div className="d-flex align-items-center mt-5">
+          <div className="flex-shrink-0">
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2019%2F01%2FJavaScript_Logo.png&f=1&nofb=1"
+              alt="js"
+              width="150"
+              height="150"
+            />
+          </div>
+          <div className="flex-grow-1 ms-3 ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste nisi
+            beatae soluta vero esse voluptatibus ratione commodi, deleniti,
+            maiores sequi eaque nemo eligendi officia illum perspiciatis
+            exercitationem rerum minus error?
+          </div>
+        </div>
+        <div className="d-flex align-items-center mt-5">
+          <div className="flex-shrink-0">
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2019%2F01%2FJavaScript_Logo.png&f=1&nofb=1"
+              alt="js"
+              width="150"
+              height="150"
+            />
+          </div>
+          <div className="flex-grow-1 ms-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste nisi
+            beatae soluta vero esse voluptatibus ratione commodi, deleniti,
+            maiores sequi eaque nemo eligendi officia illum perspiciatis
+            exercitationem rerum minus error?
+          </div>
+        </div>
+      </Container>
+    )
   }
 }
